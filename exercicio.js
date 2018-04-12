e65e26ef153ecc65faa1b686abb3646a56b05107
Sarah <sasasancei@hotmail.com>
2018-04-12 11:50:18 -0300

    exercicios dia 7

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+let senha = "10";
+let comparar = prompt("Qual e a senha?");
+
+while(senha != comparar){
+    comparar = prompt("Coloque outra senha");
+}
+
+alert("Acertou");
---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+let contador = 1;
+let simbolo = "#";
+let tamanho = 5;
+
+while (contador <= tamanho){
+    console.log(simbolo);
+    contador++;
+    simbolo = simbolo +"#";
+}; 
+
